a3059b6ca9015b58539b52f1675d69d09d6a17a8
Hanna <hania.grodzicka@gmail.com>
2018-11-18 19:13:42 +0100

    ZWS-11 (#26)
    * ZWS-11 Add type suggestions, reorder methods
    
    * ZWS-11 Clock fix: revert changes made to circular_queue

--- algorithms/lru.py
+++ algorithms/lru.py
@@ -4,6 +4,7 @@ Least Recently Used page replacement algorithm implementation
 
 import logging
 
+import page_table as pt
 import result_tuple as rt
 
 LOG = logging.getLogger(__name__)
@@ -12,124 +13,35 @@ LOG = logging.getLogger(__name__)
 class LRU:
     """
     Provides LRU page replacement algorithm implementation
-    for given table of pages and trace dataset
+    for given table of pages and trace dataset/
 
-    Hit or new item:  add to page table and mark in hash table the memory load #
-    Miss:   go through hash table, find lowest #, outright, since numbers are growing, evict it. call add again
+    Hit or new item: add to page table and mark in hash table the memory load #.
+    Miss: go through hash table, find lowest #, outright, since numbers are growing, evict it.
+          Call add again.
     """
 
-    def __init__(self, page_table, trace):
-        # set the variables for our algorithm
-        self.page_table = page_table
-        self.trace = trace
-        self.frame_list = page_table.frame_table
+    def __init__(self, page_table: pt.PageTable, trace: list):
+        self.page_table: pt.PageTable = page_table
+        self.trace: list = trace
+        self.frame_list: list = page_table.frame_table
 
         self.initialize_ppns()
 
-        # output variables
-        self.hit = False
-        self.evict = False
-        self.dirty = False
-
-    def __str__(self) -> str:
-        return 'LRU'
+        self.hit: bool = False
+        self.evict: bool = False
+        self.dirty: bool = False
 
     def initialize_ppns(self):
         """
         Assigns PPNs (Physical Page Numbers) to each frame from page_table.
         """
-        counter = 0
+        counter: int = 0
         for elem in self.frame_list:
             elem.ppn = counter
             counter += 1
 
-    def add_or_update_successful(self, vpn, read_or_write):
-        """
-        Takes care of next page in trace
-        :param vpn: virtual page number
-        :param read_or_write: read or write action
-        :return: boolean: hit == true, evict == false
-        """
-        # first check if we have a hit
-        for elem in self.frame_list:
-            if elem.vpn == vpn:
-                # mark that we had a hit
-                self.hit = True
-
-                # add the page
-                # set values accordingly
-                elem.in_use = True
-                elem.vpn = vpn
-                # if we're doing a write, need to set dirty bit
-                if read_or_write == 'W':
-                    elem.dirty = True
-                # if we have a READ, mark the last reference variable, so we know that this was the last time our
-                # selected VPN was accessed
-                else:
-                    elem.last_reference = self.page_table.total_memory_accesses
-                return True
-        else:
-            self.hit = False
-            # try and add to an empty space, even though we have a page fault
-            if not self.add_after_page_fault(vpn, read_or_write):
-                # and if that returns false, we need to EVICT and try again
-                self.evict = True
-                self.evict_page()
-
-            return False
-
-    def add_after_page_fault(self, vpn, read_or_write):
-        """
-        Adds to an empty space
-        :param vpn: virtual page number
-        :param read_or_write: read or write action
-        :return: boolean: true == page was added, false == all items are in use
-        """
-        for elem in self.frame_list:
-            if not elem.in_use:
-                elem.in_use = True
-                elem.vpn = vpn
-                # if we're doing a write, need to set dirty bit
-                if read_or_write == 'W':
-                    elem.dirty = True
-                else:
-                    # if we have a read, mark our reference
-                    elem.last_reference = self.page_table.total_memory_accesses
-                return True
-
-        # if we make it this far, then all items are in use, so return false
-        return False
-
-    def evict_page(self):
-        """
-        Gets rid of last page
-        """
-        lowest_value = None
-        lowest_value_ppn = 0
-
-        for elem in self.frame_list:
-            # index by the key to get a value
-            value = elem.last_reference
-            # find the lowest value overall, and get its PPN and VPN
-            if lowest_value is None or value < lowest_value:
-                lowest_value = value
-                lowest_value_ppn = elem.ppn
-
-        # remove the lowest value vpn
-        self.remove(lowest_value_ppn)
-
-    def remove(self, ppn):
-        """
-        Removes selected ppn from page_table
-        :param ppn: physical page number (index)
-        """
-        removal_page = self.frame_list[ppn]
-        # if the page is dirty, we need to do a disk write
-        if removal_page.dirty:
-            self.dirty = True
-        removal_page.in_use = False
-        removal_page.dirty = False
-        removal_page.vpn = None
+    def __str__(self) -> str:
+        return 'LRU'
 
     def run_algorithm(self) -> rt.ResultTuple:
         """
@@ -164,6 +76,94 @@ class LRU:
         self.print_results()
         return rt.ResultTuple(len(self.page_table.frame_table), self.page_table.total_memory_accesses,
                               self.page_table.page_faults, self.page_table.writes_to_disk, 'N/A')
+
+    def add_or_update_successful(self, vpn, read_or_write):
+        """
+        Takes care of next page in trace
+        :param vpn: virtual page number
+        :param read_or_write: read or write action
+        :return: boolean: hit == true, evict == false
+        """
+        # first check if we have a hit
+        for elem in self.frame_list:
+            if elem.vpn == vpn:
+                # mark that we had a hit
+                self.hit = True
+
+                # add the page
+                # set values accordingly
+                elem.in_use = True
+                elem.vpn = vpn
+                # if we're doing a write, need to set dirty bit
+                if read_or_write == 'W':
+                    elem.dirty = True
+                # if we have a READ, mark the last reference variable, so we know that this was the last time our
+                # selected VPN was accessed
+                else:
+                    elem.last_reference = self.page_table.total_memory_accesses
+                return True
+
+        self.hit = False
+        # try and add to an empty space, even though we have a page fault
+        if not self.add_after_page_fault(vpn, read_or_write):
+            # and if that returns false, we need to EVICT and try again
+            self.evict = True
+            self.evict_page()
+
+        return False
+
+    def add_after_page_fault(self, vpn: int, read_or_write: str):
+        """
+        Adds to an empty space
+        :param vpn: virtual page number
+        :param read_or_write: read or write action
+        :return: boolean: true == page was added, false == all items are in use
+        """
+        for elem in self.frame_list:
+            if not elem.in_use:
+                elem.in_use = True
+                elem.vpn = vpn
+                # if we're doing a write, need to set dirty bit
+                if read_or_write == 'W':
+                    elem.dirty = True
+                else:
+                    # if we have a read, mark our reference
+                    elem.last_reference = self.page_table.total_memory_accesses
+                return True
+
+        # if we make it this far, then all items are in use, so return false
+        return False
+
+    def evict_page(self):
+        """
+        Gets rid of last page
+        """
+        lowest_value = None
+        lowest_value_ppn: int = 0
+
+        for elem in self.frame_list:
+            # index by the key to get a value
+            value = elem.last_reference
+            # find the lowest value overall, and get its PPN and VPN
+            if lowest_value is None or value < lowest_value:
+                lowest_value = value
+                lowest_value_ppn = elem.ppn
+
+        # remove the lowest value vpn
+        self.remove(lowest_value_ppn)
+
+    def remove(self, ppn: int):
+        """
+        Removes selected ppn from page_table
+        :param ppn: physical page number (index)
+        """
+        removal_page = self.frame_list[ppn]
+        # if the page is dirty, we need to do a disk write
+        if removal_page.dirty:
+            self.dirty = True
+        removal_page.in_use = False
+        removal_page.dirty = False
+        removal_page.vpn = None
 
     def print_trace(self, next_address, next_vpn):
         """
@@ -200,7 +200,7 @@ class LRU:
         Prints algorithm final result
         """
         LOG.info("Algorithm: LRU")
-        LOG.info("Number of frames:   %s", str(len(self.page_table.frame_table)))
+        LOG.info("Number of frames:      %s", str(len(self.page_table.frame_table)))
         LOG.info("Total Memory Accesses: %s", str(self.page_table.total_memory_accesses))
-        LOG.info("Total Page Faults: %s", str(self.page_table.page_faults))
-        LOG.info("Total Writes to Disk: %s", str(self.page_table.writes_to_disk))
+        LOG.info("Total Page Faults:     %s", str(self.page_table.page_faults))
+        LOG.info("Total Writes to Disk:  %s", str(self.page_table.writes_to_disk))
